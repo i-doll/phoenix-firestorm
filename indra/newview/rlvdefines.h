2dfe13df5a2a816ef9452c12c077dd78ed40ac2a
Amalthea Skydancer <amalthea@th3a.dev>
2026-01-03 11:52:47 +0100

    Added custom RLV command

--- indra/newview/rlvdefines.h
+++ indra/newview/rlvdefines.h
@@ -137,6 +137,7 @@ enum ERlvBehaviour {
     RLV_BHVR_SHOWNAMES,             // "shownames"
     RLV_BHVR_SHOWNAMETAGS,          // "shownametags"
     RLV_BHVR_SHOWNEARBY,
+    RLV_BHVR_SHOWPEOPLE,            // "showpeople"
     RLV_BHVR_SHOWHOVERTEXT,         // "showhovertext"
     RLV_BHVR_SHOWHOVERTEXTHUD,      // "showhovertexthud"
     RLV_BHVR_SHOWHOVERTEXTWORLD,    // "showhovertextworld"
