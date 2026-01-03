8d62a215b8e9f29cc4e2959f6750dc84d3124a2d
Amalthea Skydancer <amalthea@th3a.dev>
2026-01-03 11:52:47 +0100

    Added custom RLV command

--- indra/newview/rlvhelper.cpp
+++ indra/newview/rlvhelper.cpp
@@ -158,6 +158,7 @@ RlvBehaviourDictionary::RlvBehaviourDictionary()
     addEntry(new RlvBehaviourProcessor<RLV_BHVR_SHOWNAMETAGS>("shownametags", RlvBehaviourInfo::BHVR_STRICT));
     addModifier(RLV_BHVR_SHOWNAMETAGS, RLV_MODIFIER_SHOWNAMETAGSDIST, new RlvBehaviourModifierHandler<RLV_MODIFIER_SHOWNAMETAGSDIST>("Name Tags - Visible Distance", 0.0f, true, new RlvBehaviourModifierCompMin));
     addEntry(new RlvBehaviourGenericToggleProcessor<RLV_BHVR_SHOWNEARBY, RLV_OPTION_NONE>("shownearby", RlvBehaviourInfo::BHVR_EXPERIMENTAL));
+    addEntry(new RlvBehaviourGenericProcessor<RLV_OPTION_NONE>("showpeople", RLV_BHVR_SHOWPEOPLE));
     addEntry(new RlvBehaviourGenericToggleProcessor<RLV_BHVR_SHOWSELF, RLV_OPTION_NONE, RlvBehaviourShowSelfToggleHandler>("showself", RlvBehaviourInfo::BHVR_EXPERIMENTAL));
     addEntry(new RlvBehaviourGenericToggleProcessor<RLV_BHVR_SHOWSELFHEAD, RLV_OPTION_NONE, RlvBehaviourShowSelfToggleHandler>("showselfhead", RlvBehaviourInfo::BHVR_EXPERIMENTAL));
     addEntry(new RlvBehaviourGenericProcessor<RLV_OPTION_NONE>("showworldmap", RLV_BHVR_SHOWWORLDMAP));
--- indra/newview/rlvui.h
+++ indra/newview/rlvui.h
@@ -48,6 +48,7 @@ protected:
     void onToggleShowLoc();                                             // showloc
     void onToggleShowNames();                                           // shownames
     void onToggleShowMinimap();                                         // showminimap
+    void onToggleShowPeople();                                          // showpeople
     void onToggleShowWorldMap();                                        // showworldmap
     void onToggleTp();                                                  // tploc and tplm
     void onToggleUnsit();                                               // unsit
