c66e256a9ce5e20bbdd296424ce50b9ddc7cadd9
Amalthea Skydancer <amalthea@th3a.dev>
2026-01-03 13:05:00 +0100

    add @lockenv=n

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -8724,6 +8724,10 @@ void LLSettingsBridge::performAction(LLInventoryModel* model, std::string action
 {
     if ("apply_settings_local" == action)
     {
+// [RLVa:ID] - @lockenv
+        if (!RlvActions::canOverrideEnvironment())
+            return;
+// [/RLVa:ID]
         // Single item only
         LLViewerInventoryItem* item = static_cast<LLViewerInventoryItem*>(getItem());
         if (!item)
@@ -8813,6 +8817,10 @@ void LLSettingsBridge::buildContextMenu(LLMenuGL& menu, U32 flags)
 
         items.push_back("Settings Separator");
         items.push_back("Settings Apply Local");
+// [RLVa:ID] - @lockenv
+        if (!RlvActions::canOverrideEnvironment())
+            disabled_items.push_back("Settings Apply Local");
+// [/RLVa:ID]
 
         items.push_back("Settings Apply Parcel");
         if (!canUpdateParcel())
