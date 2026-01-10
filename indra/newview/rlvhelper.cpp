288e1ccf1d6635d0532ecbb15a5e3589867042e8
Amalthea Skydancer <amalthea@th3a.dev>
2026-01-10 22:07:28 +0100

    Add RLVa command for forcing profile picture

--- indra/newview/rlvhelper.cpp
+++ indra/newview/rlvhelper.cpp
@@ -298,6 +298,7 @@ RlvBehaviourDictionary::RlvBehaviourDictionary()
     addEntry(new RlvForceProcessor<RLV_BHVR_SETCAM_FOV>("setcam_fov", RlvBehaviourInfo::BHVR_EXPERIMENTAL));
     addEntry(new RlvForceProcessor<RLV_BHVR_SETCAM_MODE>("setcam_mode", RlvBehaviourInfo::BHVR_EXPERIMENTAL));
     addEntry(new RlvForceProcessor<RLV_BHVR_SETGROUP>("setgroup"));
+    addEntry(new RlvForceProcessor<RLV_BHVR_SETPROFILEIMAGE>("setprofileimage")); // [RLVa:ID]
     addEntry(new RlvForceProcessor<RLV_BHVR_SIT>("sit"));
     addEntry(new RlvForceProcessor<RLV_BHVR_SITGROUND>("sitground"));
     addEntry(new RlvForceProcessor<RLV_BHVR_TPTO>("tpto"));
